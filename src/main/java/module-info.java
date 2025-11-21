fb30862bc6516c5402b28874dd2305f46235f888
SantiagoCV <vegacifuentessantiago@gmail.com>
2025-11-21 08:23:33 -0500

    estructura

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,7 @@
 module co.edu.uniquindio.clinica {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
 
     opens co.edu.uniquindio.clinica to javafx.fxml;
     exports co.edu.uniquindio.clinica.model;
